0ab0e0346f5f61d0835075e878eafba82eb994af
sergio-banuelos <cain-x@hotmail.com>
2016-07-04 01:33:32 -0500

    filtro consulta reversa

--- iapsmx/analysis/views.py
+++ iapsmx/analysis/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from poms.models import cuestionarioPOMS
+from django.http import HttpResponseRedirect, HttpResponse
+from .utils import queryset_to_workbook
+from personas.models import Persona
+#from poms.models import cuestionarioPOMS
 from analysis.models import factorPOMS
+from encuestas.models import ImagenEncuesta
+from django.db.models import StdDev, Avg
+
 
 # Create your views here.
 def index(request):
-    coleram=0
-    coleras=0
 
-    fatigam=0
-    fatigas=0
 
-    vigorm=0
-    vigors=0
 
-    amistadm=0
-    amistads=0
+    #imagen20=ImagenEncuesta.objects.select_related().filter(imagenIAPS=20)
+    #personasimg20= Persona.objects.select_related()
+    #fpoms= factorPOMS.objects.extra(tables=['encuestas_imagenencuesta'],where=['encuestas_imagenencuesta.imagenIAPS=20', 'encuestas_imagenencuesta.persona_id=analysis_factorpoms.persona_id'])
+    fpoms = factorPOMS.objects.filter(persona__imagenencuesta__imagenIAPS=20)
 
-    tensionm=0
-    tensions=0
+    coleram = fpoms.aggregate(Avg('colera'))['colera__avg']
+    fatigam = fpoms.aggregate(Avg('fatiga'))['fatiga__avg']
+    vigorm = fpoms.aggregate(Avg('vigor'))['vigor__avg']
+    amistadm = fpoms.aggregate(Avg('amistad'))['amistad__avg']
+    tensionm = fpoms.aggregate(Avg('tension'))['tension__avg']
+    depresionm = fpoms.aggregate(Avg('deprimido'))['deprimido__avg']
 
-    depresionm=0
-    depresions=0
+
+    coleras = fpoms.aggregate(StdDev('colera'))['colera__stddev']
+    fatigas = fpoms.aggregate(StdDev('fatiga'))['fatiga__stddev']
+    vigors = fpoms.aggregate(StdDev('vigor'))['vigor__stddev']
+    amistads = fpoms.aggregate(StdDev('amistad'))['amistad__stddev']
+    tensions = fpoms.aggregate(StdDev('tension'))['tension__stddev']
+    depresions = fpoms.aggregate(StdDev('deprimido'))['deprimido__stddev']
+
+
+    #factor = factorPOMS.objects.filter()
 
     #todos los poms
     #pomsmodel = cuestionarioPOMS.objects.all()
@@ -31,6 +44,20 @@ def index(request):
 
 
     if request.method == 'POST':
+
+        queryset = fpoms
+
+        columns = (
+            'amistad', 'tension'
+            )
+        workbook = queryset_to_workbook(queryset, columns)
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="export.xls"'
+        workbook.save(response)
+
+        return response
+
+
         """
         pomsmodel = cuestionarioPOMS.objects.all()
         # Attempt to grab information from the raw form information.
